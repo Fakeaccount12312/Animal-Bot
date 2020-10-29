08af209f3358dc0b8af0ee31adf961b444979fa6
Fakeaccount12312 <fake0747.0@gmail.com>
2020-10-29 23:57:12 +0100

    added github command and small code change

--- bot.py
+++ bot.py
@@ -41,20 +41,23 @@ async def on_message(message):
     if message.author == client.user:
         return
     if msg.startswith('-') and message.channel.name == "bot-test":
-        if msg == "-help animals":
+        msg = msg[1:]
+        if msg == "help animals":
             embedded = discord.Embed(title="Unterstützte Tierarten/ Befehle:", description=supported_animals)
             await message.channel.send(embed=embedded)
-        elif msg[1:] in synonyms and synonyms[msg[1:]] in data:
-            await postimage(message.channel, data[synonyms[msg[1:]]][0] + random.choice(data[synonyms[msg[1:]]]))
-        elif msg == "-random":
+        elif msg in synonyms and synonyms[msg] in data:
+            await postimage(message.channel, data[synonyms[msg]][0] + random.choice(data[synonyms[msg]]))
+        elif msg == "random":
             animal = random.choice(list(data.keys()))
             await postimage(message.channel, data[synonyms[animal]][0] + random.choice(data[synonyms[animal]]), title="A random " + animal)
-        elif msg == "-human":
+        elif msg == "human":
             await postimage(message.channel, "https://m.media-amazon.com/images/M/MV5BOTIyODY1OTYtZjAzNS00ZGQ2LWFhNmItMTJkYTc0MDNkYTk0XkEyXkFqcGdeQXVyODg3NTgyODQ@._V1_.jpg")
-        elif msg == "-update" and message.author.name == "Fakeaccount1231":
+        elif msg in ("github", "source", "sourcecode", "code"):
+            await message.channel.send(embed=discord.Embed(title="Github", url="https://github.com/Fakeaccount12312/Animal-Bot"))
+        elif msg == "update" and message.author.name == "Fakeaccount1231":
             await message.channel.send(embed=discord.Embed(title="updating..."))
             os.system("sh update.sh &")
-        elif msg == "-stop" and message.author.name == "Fakeaccount1231":
+        elif msg == "stop" and message.author.name == "Fakeaccount1231":
             await message.channel.send(embed=discord.Embed(title="Bot stopped"))
             exit()
 
